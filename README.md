18ee6ae289237f32dd8cb94e299e93550527ae77
Simon Ritchie <simonritchie.uk@gmail.com>
2019-12-12 20:17:18 +0000

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # tiler
-Tiler takes a point cloud and renders it as a gif  with shades of grey representing the height values
+Tiler takes an ESRI grid format file
+and renders it as a monochrome png
+with shades of grey representing the height values.
+
+UK Environment Agency Lidar tiles are in ESRI grid format.
+tq1652_DTM_1M.asc is their Digital Terrain Model (DTM)
+covering the one kilometre map square TQ1652
+with a cell size of one metre.
+(A DTM has as much vegetation removed as possible,
+so shows the ground surface.)
+
+For a list of options:
+
+    $ tiler -h
+
+To process the DTM file and produce tq1652.png:
+
+    $ tiler -i tq1652_DTM_1M.asc -o tq1652.png
+
+TQ1652 covers a piece of ground between Dorking and Leatherhead.
+The River Mole, the railway line and the highway are clearly visible.
+In the top right corner the land rises steadily.
+That's the lower slopes of Box Hill
+
+The tiler draws a picture with one pixel per cell.
+It uses 256 shades of grey to represent the height data in the point cloud.
+The floor is drawn in white
+and the ceiling is drawn in black.
+Intermediate points are drawn in a shade of grey.
+
+By default the floor is set to the lowest point in the file and
+the ceiling is set to the highest point,
+but you can override that.
+If you want to compare two tiles,
+you should use the same values for the floor and ceiling.
+
+The -f options sets the height of the floor,
+-c sets the ceiling.
+
---
+++ tiler.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"flag"
+	cloud "github.com/goblimey/tiler/point_cloud"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+)
+
+var filename string // The point cloud file to display.
+var output string   // The .png results file.
+var ceiling64 float64 // parameter - the maximum height expected.
+var ceiling float32	// ceiling as a float32
+var floor64 float64   // parameter - the minimum height expected.
+var floor float32	// floor as a float32
+var verbose bool    // verbose mode
+
+var maxHeight float64 = 0
+var maxHeightSet = false
+var minHeight float64 = 0
+var minHeightSet = false
+var maxShade uint8 = 0
+var maxShadeSet = false
+var minShade uint8 = 0
+var minShadeSet = false
+
+func init() {
+	flag.StringVar(&filename, "input", "", "point cloud data file")
+	flag.StringVar(&filename, "i", "", "point cloud data file")
+	flag.StringVar(&output, "output", "", ".png results file")
+	flag.StringVar(&output, "o", "", ".png results file")
+	flag.Float64Var(&ceiling64, "ceiling", 0.0, "maximum height expected")
+	flag.Float64Var(&ceiling64, "c", 0.0, "maximum height expected")
+	flag.Float64Var(&floor64, "floor", 0.0, "mimimum height expected")
+	flag.Float64Var(&floor64, "f", 0.0, "minimum height expected")
+	flag.BoolVar(&verbose, "verbose", false, "verbose mode")
+	flag.BoolVar(&verbose, "v", false, "verbose mode")
+}
+
+func main() {
+	flag.Parse()
+
+	// filename = "TT"
+	// output := "tile.png"
+
+	flagset := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { flagset[f.Name] = true })
+
+	if flagset["floor"] {
+		floor = float32(floor64)
+		minHeightSet = true
+	}
+	if flagset["f"] {
+		floor = float32(floor64)
+		minHeightSet = true
+	}
+	if flagset["ceiling"] {
+		ceiling = float32(ceiling64)
+		maxHeightSet = true
+	}
+	if flagset["c"] {
+		ceiling = float32(ceiling64)
+		maxHeightSet = true
+	}
+
+	out, err := os.Create(output)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	pc := cloud.ConcretePointCloud{}
+	err = pc.ReadPointCloudFromFile(filename, verbose)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	// If floor or ceiling not already set, set them from the data.
+	if !minHeightSet {
+		floor = pc.MinHeight() - 0.1
+	}
+
+	if !maxHeightSet {
+		ceiling = pc.MaxHeight() + 0.1
+	}
+
+	log.Printf("creating image - floor %f ceiling %f\n", floor, ceiling)
+	img := image.NewRGBA(image.Rect(0, 0, pc.Nrows(), pc.Ncols()))
+	maxRow := pc.Nrows() - 1
+	for row := maxRow; row >= 0; row-- {
+		for col := 0; col < pc.Ncols(); col++ {
+			c := shade(floor, ceiling, pc.Height(row, col))
+			if verbose {
+				log.Printf("colouring cell[%d[%d] %d\n", row, col, c)
+			}
+			img.Set(col, row, c)
+		}
+	}
+
+	log.Printf("encoding image")
+	err = png.Encode(out, img)
+
+	log.Printf("%d %d %f %f %d %d", pc.Nrows(), pc.Ncols(), pc.MinHeight(), pc.MaxHeight(), minShade, maxShade)
+}
+
+func shade(floor, ceiling, height float32) color.Color {
+	// Get height and ceiling relative to the floor.
+	height = height - floor
+	ceiling = ceiling - floor
+	shade := uint8(255 - uint8(height*256.0/ceiling))
+	if verbose {
+		log.Printf("shade %d", shade)
+	}
+	if maxShadeSet {
+		if shade > maxShade {
+			maxShade = shade
+		}
+	} else {
+		maxShade = shade
+		maxShadeSet = true
+	}
+	if minShadeSet {
+		if shade < minShade {
+			minShade = shade
+		}
+	} else {
+		minShade = shade
+		minShadeSet = true
+	}
+	return color.Gray{shade}
+}
